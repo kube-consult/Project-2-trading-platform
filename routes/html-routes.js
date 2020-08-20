27957ce2414a427787f139d3e64938ca9b3f2475
Paul Foster <paul.foster2@ibm.com>
2020-08-20 21:02:25 +1000

    updated routes added pages

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,19 +7,6 @@ const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
   app.get("/", (req, res) => {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.render("members");
-    }
-    res.render("signup");
-  });
-
-  app.get("/members", (req, res) => {
-    res.render("members");
-  });
-
-  // Sending the user to sign up page
-  app.get("/index", (req, res) => {
     res.render("index");
   });
 
@@ -31,26 +18,20 @@ module.exports = function(app) {
     res.render("login");
   });
 
-    app.get("/cards", (req, res) => {
-    // If the user already has an account send them to the members page
-    res.sendFile(path.join(__dirname, "../public/getCard.html"));
-  });
-
-      app.get("/userSummery", (req, res) => {
-    // If the user already has an account send them to the members page
-    res.sendFile(path.join(__dirname, "../public/userSummery.html"));
-  });
-      app.get("/signup", (req, res) => {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
+  app.get("/cards", (req, res) => {
+    if (!req.user) {
+      res.render("login");
+    } else {
+      res.render("cards");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, (req, res) => {
-    res.render("members");
+  app.get("/userSummery", (req, res) => {
+    if (req.user) {
+      res.render("userSummery");
+    } else {
+      res.render("login");
+    }
   });
+
 };
